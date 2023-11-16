4fb008c3d01ba40506c9653c50a07089c25ed21e
Michał Komor <mkomor@sdncenter.pl>
2023-11-16 08:25:55 +0100

    main.py updated

--- Python_langchain_script/main.py
+++ Python_langchain_script/main.py
@@ -2,7 +2,7 @@ from langchain.llms import OpenAI
 
 if __name__ == '__main__':
     llm = OpenAI()
-    question = "What can i do in Warsaw?"
-    response = llm.invoke(question)
+    prompt = "Say hello to my CodeDay Audience"
+    response = llm.invoke(prompt)
     print(response)
 
